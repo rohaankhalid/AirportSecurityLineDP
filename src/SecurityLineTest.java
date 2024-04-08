104628151c1f7b1171acbf89b688a435cbd2735d
rohaankhalid <rohaankhalid10@gmail.com>
2024-04-08 06:21:41 -0500

    added comments

--- src/SecurityLineTest.java
+++ src/SecurityLineTest.java
@@ -11,6 +11,7 @@ public class SecurityLineTest {
         line.addPerson(new Person("Captain Shaheer", PersonType.FLIGHT_STAFF));
         line.addPerson(new Person("Khalid", PersonType.VIP));
 
+        //flight staff first, then VIP, then economy
         assertEquals(PersonType.FLIGHT_STAFF, line.processNext().type);
         assertEquals(PersonType.VIP, line.processNext().type);
         assertEquals(PersonType.ECONOMY, line.processNext().type);
@@ -22,6 +23,7 @@ public class SecurityLineTest {
         line.addPerson(new Person("Imran Khan", PersonType.VIP));
         line.addPerson(new Person("Babar Azam", PersonType.VIP));
 
+        //if same types in line, the one that joined the queue first is processed first
         assertEquals("Imran Khan", line.processNext().name);
         assertEquals("Babar Azam", line.processNext().name);
     }
